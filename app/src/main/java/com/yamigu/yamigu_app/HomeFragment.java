fc051f240dc4058f835769364727811b310292f7
Hwancheol Kang <khc146@gmail.com>
2019-07-31 23:36:03 +0900

    update button animation

--- app/src/main/java/com/yamigu/yamigu_app/HomeFragment.java
+++ app/src/main/java/com/yamigu/yamigu_app/HomeFragment.java
@@ -15,10 +15,11 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeFragment extends Fragment {
     private Toolbar tb;
-    private Button btn_yamigu;
+    private ImageButton btn_yamigu;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,32 +29,13 @@ public class HomeFragment extends Fragment {
         ((AppCompatActivity)getActivity()).getSupportActionBar().setElevation(0);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
         setHasOptionsMenu(true);
-        //final Animation animAlpha = AnimationUtils.loadAnimation(view.getContext(), R.anim.anim_alpha);
-        final Animation animAlpha = new AlphaAnimation(1.0f, 0.5f);
-        animAlpha.setDuration(300);
-        animAlpha.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                return;
-            }
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                Intent intent = new Intent(getActivity(), MeetingApplicationActivity.class);
-                startActivity(intent);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                return;
-            }
-        });
-        btn_yamigu = (Button) view.findViewById(R.id.btn_yamigu);
+        btn_yamigu = (ImageButton) view.findViewById(R.id.btn_yamigu);
         btn_yamigu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                view.startAnimation(animAlpha);
-
+                Intent intent = new Intent(getActivity(), MeetingApplicationActivity.class);
+                startActivity(intent);
             }
         });
 
