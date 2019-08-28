ad7fbee113da682726e344b4ce274a3dfd2cd17c
khc196 <khc146@gmail.com>
2019-08-28 21:36:47 +0900

    Login Process update

--- app/src/main/java/com/yamigu/yamigu_app/LoginActivity.java
+++ app/src/main/java/com/yamigu/yamigu_app/LoginActivity.java
@@ -1,38 +1,52 @@
 package com.yamigu.yamigu_app;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.kakao.auth.AuthType;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
 import com.kakao.util.exception.KakaoException;
 import com.kakao.util.helper.log.Logger;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class LoginActivity extends AppCompatActivity {
+
+
     ImageButton btn_login_kakao;
     private LoginButton btn_kakao_login;
     private SessionCallback callback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        callback = new SessionCallback();
-        Session.getCurrentSession().addCallback(callback);
-        Session.getCurrentSession().checkAndImplicitOpen();
+//        callback = new SessionCallback();
+//        Session.getCurrentSession().addCallback(callback);
+//        Session.getCurrentSession().checkAndImplicitOpen();
         btn_login_kakao = (ImageButton) findViewById(R.id.btn_login_kakao);
 
         btn_login_kakao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btn_kakao_login.performClick();
-                //Intent intent = new Intent(getApplicationContext(), VerificationActivity.class);
-                //startActivity(intent);
-                //overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_fadeout_short);
-
+                Session session = Session.getCurrentSession();
+                session.addCallback(new SessionCallback());
+                session.open(AuthType.KAKAO_LOGIN_ALL, LoginActivity.this);
             }
         });
         btn_kakao_login = (LoginButton) findViewById(R.id.btn_kakao_login);
@@ -52,18 +66,48 @@ public class LoginActivity extends AppCompatActivity {
         Session.getCurrentSession().removeCallback(callback);
     }
 
-    private class SessionCallback implements ISessionCallback {
-
+    public class SessionCallback implements ISessionCallback {
+        // 로그인에 성공한 상태
         @Override
         public void onSessionOpened() {
-            redirectSignupActivity();
+            requestMe();
         }
-
+        // 로그인에 실패한 상태
         @Override
         public void onSessionOpenFailed(KakaoException exception) {
-            if(exception != null) {
-                Logger.e(exception);
-            }
+            Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
+        }
+        // 사용자 정보 요청
+        public void requestMe() {
+            // 사용자정보 요청 결과에 대한 Callback
+            UserManagement.getInstance().requestMe(new MeResponseCallback() {
+                // 세션 오픈 실패. 세션이 삭제된 경우,
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                    Log.e("SessionCallback :: ", "onSessionClosed : " + errorResult.getErrorMessage());
+                }
+                // 회원이 아닌 경우,
+                @Override
+                public void onNotSignedUp() {
+                    Log.e("SessionCallback :: ", "onNotSignedUp");
+                }
+                // 사용자정보 요청에 성공한 경우,
+                @Override
+                public void onSuccess(UserProfile userProfile) {
+                    String url = "http://192.168.43.223:9999/api/oauth/kakao/";
+                    String access_token = Session.getCurrentSession().getTokenInfo().getAccessToken();
+                    ContentValues values = new ContentValues();
+                    values.put("access_token", access_token);
+                    //values.put("kakao_account", userProfile.toString());
+                    NetworkTask networkTask = new NetworkTask(url, values);
+                    networkTask.execute();
+                }
+                // 사용자 정보 요청 실패
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    Log.e("SessionCallback :: ", "onFailure : " + errorResult.getErrorMessage());
+                }
+            });
         }
     }
 
@@ -72,9 +116,103 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+    protected void redirectMainActivity() {
+        final Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
     @Override
     public void onBackPressed() {
         finish();
         overridePendingTransition(R.anim.anim_fadein, R.anim.anim_slide_out_right);
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+        private RequestHttpURLConnection requestHttpURLConnection;
+        public NetworkTask(String url, ContentValues values) {
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values, "POST", ""); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            String url = "http://192.168.43.223:9999/api/user/info/";
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(s);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            ContentValues values = new ContentValues();
+            try {
+                NetworkTask2 networkTask2 = new NetworkTask2(url, values, jsonObject.getString("key"));
+                Log.d("onPostExecute :: ", "jsonObject key: " + jsonObject.getString("key"));
+                networkTask2.execute();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    public class NetworkTask2 extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+        private RequestHttpURLConnection requestHttpURLConnection;
+        private String token;
+        private Boolean isFirstRun;
+        public NetworkTask2(String url, ContentValues values, String token) {
+            this.url = url;
+            this.values = values;
+            this.token = token;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String result; // 요청 결과를 저장할 변수.
+            requestHttpURLConnection = new RequestHttpURLConnection();
+
+            result = requestHttpURLConnection.request(url, values, "GET", token); // 해당 URL로 부터 결과물을 얻어온다.
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            JSONObject jsonObject = null;
+            boolean signup_flag = false;
+            try {
+                jsonObject = new JSONObject(s);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            signup_flag = jsonObject.isNull("gender") || jsonObject.isNull("nickname") || jsonObject.isNull("phone") || jsonObject.isNull("belong") || jsonObject.isNull("department") || jsonObject.isNull("age") || jsonObject.isNull("email");
+            isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isFirstRun", true);
+
+            if (!isFirstRun) {
+                if(signup_flag) {
+                    redirectSignupActivity();
+                }
+                else {
+                    redirectMainActivity();
+                }
+            }
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("isFirstRun", false).commit();
+
+        }
     }
 }
